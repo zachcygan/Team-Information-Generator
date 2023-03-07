8adc35c4a1444f83b49fe9c2b26a074e5d2d3116
Zachary Cygan <zachcygan@gmail.com>
2023-03-06 17:29:03 -0800

    changed a string to a number in manager.test.js

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,19 +2,19 @@ const Manager = require('../lib/Manager');
 
 describe('Manager', () => {
     it('Should return the Managers name', () => {
-        const manager = new Manager('Zach', 1, 'zachcygan@gmail.com', '101')
+        const manager = new Manager('Zach', 1, 'zachcygan@gmail.com', 101)
 
         expect(manager.getName()).toEqual('Zach');
     })
 
     it('Should return the Managers id', () => {
-        const manager = new Manager('Zach', 1, 'zachcygan@gmail.com', '101')
+        const manager = new Manager('Zach', 1, 'zachcygan@gmail.com', 101)
 
         expect(manager.getId()).toEqual(1);
     })
 
     it('Should return the Managers email', () => {
-        const manager = new Manager('Zach', 1, 'zachcygan@gmail.com', '101')
+        const manager = new Manager('Zach', 1, 'zachcygan@gmail.com', 101)
 
         expect(manager.getEmail()).toEqual('zachcygan@gmail.com');
     })
@@ -26,7 +26,7 @@ describe('Manager', () => {
     })
 
     it('Should return the Managers role', () => {
-        const manager = new Manager('Zach', 1, 'zachcygan@gmail.com', '101')
+        const manager = new Manager('Zach', 1, 'zachcygan@gmail.com', 101)
 
         expect(manager.getRole()).toEqual('Manager');
     })
